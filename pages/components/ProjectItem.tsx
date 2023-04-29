df05ee1069a6a74af24b4e2f3b8d1e332bdd2e2d
Samy951 <126424578+Samy951@users.noreply.github.com>
2023-04-29 18:15:50 +0200

    some bugs fixed and some deprecated elements changed

--- pages/components/ProjectItem.tsx
+++ pages/components/ProjectItem.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -10,11 +9,11 @@ const ProjectItem = ({title, backgroundImg, projectUrl} :
   )  => {
   return (
     <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-black rounded-xl p-4 group hover:bg-gradient-to-r from-[#36344A] to-[#36344A]'>
-                    <Image className="rounded-xl group-hover:opacity-20" src={backgroundImg} alt="#"/>
+                    <Image className="rounded-xl group-hover:opacity-20" src={backgroundImg}  alt={"Background"}/>
                     <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
                         <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
                         <p className='pb-4 pt-2 text-white text-center'>Nice to see your there !</p>
-                        <Link href={projectUrl}>
+                        <Link href={projectUrl ?? "projectUrl"}>
                             <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
                         </Link>
                     </div>
