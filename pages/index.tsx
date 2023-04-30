6d8ad18c7f67e2911315ae7672218b81f64f83a6
Icham M'madi <126263598+shammy-wst@users.noreply.github.com>
2023-04-30 21:09:06 +0200

    fix sementic

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,12 +18,20 @@ export default function Home() {
           <link rel="stylesheet" href="https://use.typekit.net/fqg2pyf.css"/>
         <meta name='description' content='Icham Front-End Developer' />
       </Head>
-      <Main />
-      <About />
-      <Skills />
-      <Projects />
-        <Resume />
-      <Contact />
+        <main>
+            <Main />
+                <section>
+                    <About />
+                    <Skills />
+                    <Projects />
+                </section>
+        </main>
+            <aside>
+                <Resume />
+                <Contact />
+            </aside>
+
+
 
     </div>
   )
