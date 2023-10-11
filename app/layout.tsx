60603f6e64daf72ff807522551276273eb809343
Icham M'madi <126263598+shammy-wst@users.noreply.github.com>
2023-10-11 20:02:58 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { JetBrains_Mono } from 'next/font/google'
 const inter = JetBrains_Mono({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Icham M\'MADI',
+  title: 'Shammy',
   description: 'Icham M\'MADI\'s very cool portfolio',
 }
 
