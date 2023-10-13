b66dbe028f82db92dd1f1560d984b583a89a8bae
Icham M'madi <126263598+shammy-wst@users.noreply.github.com>
2023-10-13 12:10:40 +0200

    updt bg

--- app/components/Videoloop.tsx
+++ app/components/Videoloop.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Videoloop = () => {
   return (
     <video autoPlay loop muted playsInline className='flex absolute inset-0 w-full h-full object-cover'>
-      <source src="https://github.com/shammy-wst/portfolio-shammy/blob/fixes/public/videos/background.mp4" type="video/mp4" />
+      <source src="https://github.com/shammy-wst/portfolio-shammy/public/videos/background.mp4" type="video/mp4" />
     </video>
   )
 }
