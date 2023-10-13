b778a583508b33945c2feb08beac984c53494cf3
Icham M'madi <126263598+shammy-wst@users.noreply.github.com>
2023-10-13 14:03:17 +0200

    responsive fixes

--- app/components/Videoloop.tsx
+++ app/components/Videoloop.tsx
@@ -3,9 +3,12 @@ import React from 'react'
 const Videoloop = () => {
   return (
     <div className='relative h-screen sm:h-1/2 md:h-full'>
+        
         <video autoPlay loop muted playsInline className='flex inset-0 w-screen h-screen object-cover'>
         <source src="/videos/background.mp4" type="video/mp4" />
         </video>
+        <div className='relative z-10'></div>
+        <div className="absolute inset-0 bg-black opacity-80 after:absolute after:inset-0 content-['']"></div>
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,24 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      'sm': '1px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '500px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+
+      '3xl': '1920px',
+    },
     extend: {
       animation: {
         'bounceSlow': 'bounce 3s linear infinite',
