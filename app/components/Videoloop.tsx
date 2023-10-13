cf531f4be95eae4d961b8eabe10b276b9034070e
Icham M'madi <126263598+shammy-wst@users.noreply.github.com>
2023-10-13 15:07:42 +0200

    Update Videoloop.tsx

--- app/components/Videoloop.tsx
+++ app/components/Videoloop.tsx
@@ -8,7 +8,7 @@ const Videoloop = () => {
         <source src="/videos/background.mp4" type="video/mp4" />
         </video>
         <div className='relative z-10'></div>
-        <div className="absolute inset-0 bg-black opacity-80 after:absolute after:inset-0 content-['']"></div>
+        <div className="absolute inset-0 h-screen w-screen object-cover bg-black opacity-80 after:absolute after:inset-0 content-['']"></div>
     </div>
   )
 }
