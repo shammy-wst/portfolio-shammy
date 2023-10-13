12c9699d01e14995d618f3d0ec16f2d4cf6bdef3
Icham M'madi <126263598+shammy-wst@users.noreply.github.com>
2023-10-13 13:42:17 +0200

    fix bg

--- app/components/Videoloop.tsx
+++ app/components/Videoloop.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Videoloop = () => {
   return (
-    <video autoPlay loop muted playsInline className='flex inset-0 w-full h-full object-cover'>
-      <source src="/videos/background.mp4" type="video/mp4" />
-    </video>
+    <div className='relative h-screen sm:h-1/2 md:h-full'>
+        <video autoPlay loop muted playsInline className='flex inset-0 w-screen h-screen object-cover'>
+        <source src="/videos/background.mp4" type="video/mp4" />
+        </video>
+    </div>
   )
 }
 
