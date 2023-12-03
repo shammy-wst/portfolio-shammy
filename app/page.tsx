b28d7286d5b4f8449fb60eb8dc15491c4ebb9cbd
Icham M'madi <126263598+shammy-wst@users.noreply.github.com>
2023-12-03 05:26:16 +0100

    feat:sfx

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,15 @@ import Image from "next/image";
 import MainComponent from "./components/MainComponent";
 import Header from "./components/Header";
 import NavMenu from "./components/NavMenu";
+import MusicPlayer from "./components/MusicPlayer";
 
 export default function Home() {
   return (
     <main>
+      <MusicPlayer />
       <Header />
       <MainComponent />
+      
     </main>
   );
 }
